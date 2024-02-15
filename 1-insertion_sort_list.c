d0ba934871cfcb34cace43cbad15cb1d4a8dec84
Madani00 <mamchart@gmail.com>
2024-02-15 19:14:12 +0100

    task number 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * @list: list of nodes
+ * 
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node = NULL, *tmp = NULL;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+
+	node = *list;
+	while (node)
+	{
+		while (node->next && (node->n > node->next->n))
+		{
+			tmp = node->next;
+			node->next = tmp->next;
+			tmp->prev = node->prev;
+			if (node->prev != NULL)
+				node->prev->next = tmp;
+			if (tmp->next != NULL)
+				tmp->next->prev = node;
+			node->prev = tmp;
+			tmp->next = node;
+			if (tmp->prev)
+				node = tmp->prev;
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+
+		node = node->next;
+	}
+}
