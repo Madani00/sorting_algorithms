987f4fd10a34da689233f2ea01345f7b3315506a
Madani00 <mamchart@gmail.com>
2024-02-19 21:26:51 +0100

    fix: added if condition before both swap

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -51,14 +51,19 @@ int partition(int *array, int low, int high, size_t size)
         if (array[j] < pivot)
         {
             i++;
-            swap(&array[i], &array[j]);
+            if (i != j)
+            {
+                swap(&array[i], &array[j]);
+                print_array(array, size);
+            }
         }
     
     }
-
-    swap(&array[i + 1], &array[high]);
-    print_array(array, size);
-    
+    if (i + 1 != high)
+    {
+        swap(&array[i + 1], &array[high]);
+        print_array(array, size);
+    }
 
     return (i + 1);
 }
